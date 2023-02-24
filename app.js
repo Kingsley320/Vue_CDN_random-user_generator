074ec801746d6f171b7af16f0cd9a34ab37b4c46
Kingsley320 <75090590+Kingsley320@users.noreply.github.com>
2023-02-24 19:15:02 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const app = Vue.createApp({
+    // template: '<h1>{{firstname}}</h1>',
+    data(){
+        return{
+            firstname: "Seyi",
+            lastname: 'Owolabi',
+            email: 'seyiowolabi@gmail.com',
+            gender: 'male',
+            profile: 'https://randomuser.me/api/portraits/men/30.jpg'
+        }
+    },
+    methods: {
+        async getUserData(){
+            const res = await fetch('https://randomuser.me/api/')
+            const {results} = await res.json()
+            
+            // console.log(results);
+
+            this.firstname = results[0].name.first,
+            this.lastname = results[0].name.last,
+            this.email = results[0].email,
+            this.gender = results[0].gender,
+            this.profile = results[0].picture.large
+        }
+    }
+})
+
+app.mount('#app');
